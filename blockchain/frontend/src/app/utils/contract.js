67510dd78910bdc903107c891ddeb568fe0e38d6
ranvie <neodyte@gmail.com>
2025-05-08 21:13:09 +0700

    main

--- blockchain/frontend/src/app/utils/contract.js
+++
@@ -1,7 +0,0 @@
-import web3 from './web3';
-import LandRegistryPoC from '../contracts/LandRegistryPoC.json';
-
-const address = '0xdb75066e495fbAed8D930c868Acf65A9775f536d';
-const instance = new web3.eth.Contract(LandRegistryPoC.abi, address);
-
-export default instance;
